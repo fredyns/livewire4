8421cba5503b8ec43bf8d56f55022e3e70f09dda
Fredy Nurman Saleh <email@fredyns.net>
2026-02-03 22:12:51 +0700

    apply permission checks

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,42 +2,51 @@
 
 namespace App\Policies;
 
+use App\Enums\UserRole;
 use App\Models\User;
 
 class UserPolicy
 {
     public function viewAny(User $user): bool
     {
-        return true; // todo: apply permission check
+        if ($user->hasRole(UserRole::SUPER_ADMIN->value)) {
+            return true;
+        }
+
+        return $user->can('users.index');
     }
 
     public function view(User $user, User $model): bool
     {
-        return true;
+        return $user->can('users.show');
     }
 
     public function create(User $user): bool
     {
-        return true;
+        return $user->can('users.create');
     }
 
     public function update(User $user, User $model): bool
     {
-        return true;
+        return $user->can('users.update');
     }
 
     public function delete(User $user, User $model): bool
     {
-        return $user->id != $model->id;
+        return ($user->id != $model->id) && $user->can('users.delete');
     }
 
     public function restore(User $user, User $model): bool
     {
-        return true;
+        if ($user->hasRole(UserRole::SUPER_ADMIN->value)) {
+            return true;
+        }
+
+        return $user->can('users.delete');
     }
 
     public function forceDelete(User $user, User $model): bool
     {
-        return $user->id != $model->id;
+        return ($user->id != $model->id) && $user->can('users.delete');
     }
 }
