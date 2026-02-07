7975089c5b305058575fbf9b8918bbb7e0f51678
Fredy Nurman Saleh <email@fredyns.net>
2026-02-08 00:19:18 +0700

    migrate sidebar navigation to layout components, restructuring for improved consistency

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -71,27 +71,7 @@
 {{--            </flux:sidebar.nav>--}}
         </flux:sidebar>
 
-        <flux:main container>
-            <div class="flex max-md:flex-col items-start">
-                <div class="w-full md:w-[220px] pb-4 me-10">
-                    <flux:navlist>
-                        <flux:n avlist.item href="#" current>Dashboard</flux:navlist.item>
-                        <flux:navlist.item href="#" badge="32">Orders</flux:navlist.item>
-                        <flux:navlist.item href="#">Catalog</flux:navlist.item>
-                        <flux:navlist.item href="#">Payments</flux:navlist.item>
-                        <flux:navlist.item href="#">Customers</flux:navlist.item>
-                        <flux:navlist.item href="#">Billing</flux:navlist.item>
-                        <flux:navlist.item href="#">Quotes</flux:navlist.item>
-                        <flux:navlist.item href="#">Configuration</flux:navlist.item>
-                    </flux:navlist>
-                </div>
-
-                <flux:separator class="md:hidden" />
-
-                {{ $slot }}
-
-            </div>
-        </flux:main>
+        {{ $slot }}
 
         @fluxScripts
     </body>
