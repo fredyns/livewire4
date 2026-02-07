1397215264f07cd046e0d91c650f2c1829f80534
Fredy Nurman Saleh <email@fredyns.net>
2026-02-08 00:35:22 +0700

    basic top nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,5 @@
 <x-layouts::app.header :title="$title ?? null">
     <flux:main container>
-        <div class="flex max-md:flex-col items-start">
-            <div class="w-full md:w-[220px] pb-4 me-10">
-                <flux:navlist>
-                    <flux:navlist.item href="#" current>Dashboard</flux:navlist.item>
-                    <flux:navlist.item href="#" badge="32">Orders</flux:navlist.item>
-                    <flux:navlist.item href="#">Catalog</flux:navlist.item>
-                    <flux:navlist.item href="#">Payments</flux:navlist.item>
-                    <flux:navlist.item href="#">Customers</flux:navlist.item>
-                    <flux:navlist.item href="#">Billing</flux:navlist.item>
-                    <flux:navlist.item href="#">Quotes</flux:navlist.item>
-                    <flux:navlist.item href="#">Configuration</flux:navlist.item>
-                </flux:navlist>
-            </div>
-
-            <flux:separator class="md:hidden" />
-
-            {{ $slot }}
-
-        </div>
+        {{ $slot }}
     </flux:main>
 </x-layouts::app.header>
