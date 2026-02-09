c8fa9a5ab85d3007af562d2093b9c66b490eb928
Fredy Nurman Saleh <email@fredyns.net>
2026-02-10 02:52:35 +0700

    logo to left

--- resources/views/layouts/flux.blade.php
+++ resources/views/layouts/flux.blade.php
@@ -6,26 +6,28 @@
 <body class="min-h-screen bg-white text-zinc-900 dark:bg-zinc-900 dark:text-white">
 <div class="min-h-screen flex flex-col lg:grid lg:grid-rows-[auto_1fr] lg:grid-cols-[18rem_1fr] lg:[grid-template-areas:'header_header''sidebar_main']">
     <header class="sticky top-0 z-30 flex items-center [:where(&)]:bg-white dark:[:where(&)]:bg-zinc-900 [:where(&)]:border-b [:where(&)]:border-zinc-200 dark:[:where(&)]:border-white/10 lg:[grid-area:header]" style="height: 4.5rem;">
-        <div class="mx-auto w-full h-full [:where(&)]:max-w-7xl px-6 lg:px-8 flex items-center">
-            <x-app-logo href="{{ route('home') }}" wire:navigate/>
+        <div class="w-full h-full px-6 lg:px-0 flex items-center">
+            <div class="flex items-center lg:w-[18rem] lg:px-8">
+                <x-app-logo href="{{ route('home') }}" wire:navigate/>
+            </div>
 
-            <div class="flex-1"></div>
+            <div class="flex-1 flex items-center justify-end lg:px-8">
+                <flux:button
+                    x-data
+                    x-on:click="$flux.dark = ! $flux.dark"
+                    icon="moon"
+                    variant="subtle"
+                    aria-label="Toggle dark mode"
+                />
 
-            <flux:button
-                x-data
-                x-on:click="$flux.dark = ! $flux.dark"
-                icon="moon"
-                variant="subtle"
-                aria-label="Toggle dark mode"
-            />
+                <div class="ms-2"></div>
 
-            <div class="ms-2"></div>
-
-            @if(auth()->user())
-                <x-desktop-user-menu/>
-            @else
-                <x-desktop-guest-menu/>
-            @endif
+                @if(auth()->user())
+                    <x-desktop-user-menu/>
+                @else
+                    <x-desktop-guest-menu/>
+                @endif
+            </div>
         </div>
     </header>
 
