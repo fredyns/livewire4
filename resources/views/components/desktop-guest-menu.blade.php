d00ea982bee9b0c7e6167740d797939c6eb1e776
Fredy Nurman Saleh <email@fredyns.net>
2026-02-16 07:05:46 +0700

    remove guest menu avatar and separator

--- resources/views/components/desktop-guest-menu.blade.php
+++ resources/views/components/desktop-guest-menu.blade.php
@@ -5,17 +5,6 @@
     />
 
     <flux:menu>
-        <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
-            <flux:avatar
-                name="Guest"
-                initials="G"
-            />
-            <div class="grid flex-1 text-start text-sm leading-tight">
-                <flux:heading class="truncate">Guest</flux:heading>
-                <flux:text class="truncate">anonymous</flux:text>
-            </div>
-        </div>
-        <flux:menu.separator/>
         <flux:menu.radio.group>
             <flux:menu.item :href="route('login')" icon="cog" wire:navigate>
                 {{ __('Login') }}
