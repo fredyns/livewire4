16dde8b8212c44c34e889ecb14b63bd30f5ffa85
Fredy Nurman Saleh <email@fredyns.net>
2026-02-19 13:55:52 +0700

    add middleware to sample items

--- routes/web.php
+++ routes/web.php
@@ -63,13 +63,17 @@ if (! app()->isProduction()) {
     Route::livewire('/blank2', 'pages::sample.blank2')->name('sample.blank2');
     Route::livewire('/blank3', 'pages::sample.blank3')->name('sample.blank3');
 
-    // Sample - Items
-    Route::livewire('/sample/items', 'pages::sample.item.index')->name('sample.item.index');
-    Route::livewire('/sample/items/create', 'pages::sample.item.create')->name('sample.item.create');
-    Route::livewire('/sample/items/{item}/edit', 'pages::sample.item.edit')->name('sample.item.edit');
-    Route::livewire('/sample/items/{item}', 'pages::sample.item.show')->name('sample.item.show');
-
     Route::middleware(['guest'])->group(function () {
         Route::livewire('/login-as', 'pages::auth.login-as')->name('login-as');
+    });
+
+    Route::middleware(['auth'])->group(function () {
+
+        // Sample - Items
+        Route::livewire('/sample/items', 'pages::sample.item.index')->name('sample.item.index');
+        Route::livewire('/sample/items/create', 'pages::sample.item.create')->name('sample.item.create');
+        Route::livewire('/sample/items/{item}/edit', 'pages::sample.item.edit')->name('sample.item.edit');
+        Route::livewire('/sample/items/{item}', 'pages::sample.item.show')->name('sample.item.show');
+
     });
 }
